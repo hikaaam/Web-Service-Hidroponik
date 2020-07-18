c2e45e02a46d20578f52fb337cf9db4d9eb794b7
ilyas <ilyasyusuf018@gmail.com>
2020-07-18 14:38:38 +0700

    18 july 2020

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ var router = require('express').Router();
 
 
 var routes = require('./Routes/Routes');
-process.on('uncaughtException', function(ex) {
-    console.log(""+ex);
+process.on('uncaughtException', function (ex) {
+    console.log("" + ex);
 });
 var server = app.listen(4000, function () {
     console.log('listening to request on port 4000')
@@ -53,9 +53,9 @@ var clients = [];
 var currentData = [];
 io.on('connection', function (socket) {
     console.log('\nmade socket connection', socket.id);
-    
+
     socket.on('new user', function (data) {
-        console.log('data : '+data);
+        console.log('data : ' + data);
         if (data in clients) {
             delete clients[data];
             socket.nickname = data;
@@ -63,30 +63,30 @@ io.on('connection', function (socket) {
         } else {
             socket.nickname = data;
             clients[socket.nickname] = socket;
-            
+
         }
         // setTimeout(function () {
-            if(currentData.hasOwnProperty('P'+socket.nickname)){
-                io.to(clients[socket.nickname]['id']).emit('temp',{
-                    val: currentData['P'+socket.nickname]['temp']['data'],
-                    msg: 'Current Data',
-                } );
-                io.to(clients[socket.nickname]['id']).emit('tds',{
-                    val: currentData['P'+socket.nickname]['tds']['data'],
-                    msg: 'Current Data',
-                } );
-                io.to(clients[socket.nickname]['id']).emit('wl',{
-                    val: currentData['P'+socket.nickname]['wl']['data'],
-                    msg: 'Current Data',
-                } );
-                console.log('\nupdate for user\n');
-            }
-            else{
-                console.log("\noffline\n");
-                console.log(currentData);
-            }
+        if (currentData.hasOwnProperty('P' + socket.nickname)) {
+            io.to(clients[socket.nickname]['id']).emit('temp', {
+                _val: currentData['P' + socket.nickname]['temp']['data'],
+                _msg: 'Current Data',
+            });
+            io.to(clients[socket.nickname]['id']).emit('tds', {
+                _val: currentData['P' + socket.nickname]['tds']['data'],
+                _msg: 'Current Data',
+            });
+            io.to(clients[socket.nickname]['id']).emit('wl', {
+                _val: currentData['P' + socket.nickname]['wl']['data'],
+                _msg: 'Current Data',
+            });
+            console.log('\nupdate for user\n');
+        }
+        else {
+            console.log("\noffline\n");
+            console.log(currentData);
+        }
         // }, 1000);  
-        
+
     })
 
     socket.on('chat', function (data) {
@@ -100,34 +100,39 @@ io.on('connection', function (socket) {
         console.log(msg);
         io.emit("chat message", msg);
     });
-    socket.on('temp', function (id, data) {
-       console.log('\n\nit is working bro');
-       console.log(id);
+    socket.on('temp', function (data) {
+           console.log('\n\n test');
+        //    console.log(data._id);
+        console.log(data);
+        var id = data._id;
+        console.log('\n\n' + id);
         if (clients.hasOwnProperty(id)) {
             io.to(clients[id]['id']).emit('temp', data);
         } else {
             console.log("\nTarget Device Is Offline or Doesn't exist ");
         }
-   
+
     });
-    socket.on('createData',function (data){
+    socket.on('createData', function (data) {
         if (currentData.hasOwnProperty(socket.nickname)) {
-        currentData.push(data);
-        console.log('\ndata Created');
+            currentData.push(data);
+            console.log('\ndata Created');
         } else {
-            currentData[socket.nickname]=data[socket.nickname];
+            currentData[socket.nickname] = data[socket.nickname];
             console.log('\ndata Updated\n');
         }
     });
-    socket.on('currentData', function (id,name,data) {
-       
+    socket.on('currentData', function (data) {
+        console.log(data);
+        var id = data._id;
+        var name = data._name;
         if (currentData.hasOwnProperty(id)) {
-           currentData[id][name] = data;
-           console.log('\ndata '+name+' updated\n');
+            currentData[id][name] = data;
+            console.log('\ndata ' + name + ' updated\n');
         } else {
             console.log("\nTarget Device Is Offline or Doesn't exist ");
         }
-   
+
     });
     socket.on('disconnect', function (data) {
 
@@ -135,23 +140,25 @@ io.on('connection', function (socket) {
         delete currentData[socket.nickname];
 
     });
-    socket.on('tds', function (id,data) {
+    socket.on('tds', function (data) {
+        id = data._id;
         if (clients.hasOwnProperty(id)) {
             io.to(clients[id]['id']).emit('tds', data);
             console.log(clients[id]['id']);
         } else {
             console.log("\nTarget Device Is Offline or Doesn't exist ");
         }
-      
+
     });
-    socket.on('wl', function (id,data) {
+    socket.on('wl', function (data) {
+        id = data._id;
         if (clients.hasOwnProperty(id)) {
             io.to(clients[id]['id']).emit('wl', data);
             console.log(clients[id]['id']);
         } else {
             console.log("\nTarget Device Is Offline or Doesn't exist ");
         }
-     
+
     });
 });
 
